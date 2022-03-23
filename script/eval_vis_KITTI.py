correct evaluation, add rotation ate

script/eval_vis_KITTI.py
@@ -12,16 +12,17 @@ import open3d as o3d
 from dataset_loader import KITTI
 import utils
 
-def add_y_coord_for_evaluation(pred_pos_DM):
+def add_z_coord_for_evaluation(pred_pos_DM):
     """
     pred_pos_DM (predicted position) estimated from DeepMapping only has x and z coordinate
-    convert this to <x,y=0,z> for evaluation
+    convert this to <x,y,z=0,theta> for evaluation
     """
     n = pred_pos_DM.shape[0]
     x = pred_pos_DM[:,0]
-    y = np.zeros_like(x)
-    z = pred_pos_DM[:,1]
-    return np.stack((x,y,z),axis=-1)
+    y = pred_pos_DM[:,1]
+    z = np.zeros_like(x)
+    theta = pred_pos_DM[:,2]
+    return np.stack((x,y,z,theta),axis=-1)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c','--checkpoint_dir',type=str,required=True,help='path to results folder')
@@ -36,20 +37,20 @@ traj = train_opt['traj']
 # load ground truth poses
 dataset = KITTI(data_dir,traj,voxel_size)
 gt_pose = dataset.gt_pose
-gt_location = gt_pose[:,:3]
+gt_pose_w_z = add_z_coord_for_evaluation(gt_pose)
 
 # load predicted poses
 pred_file = os.path.join(opt.checkpoint_dir,'pose_est.npy')
 pred_pose = np.load(pred_file)
-pred_location = pred_pose[:,:2]
-pred_location = add_y_coord_for_evaluation(pred_location)
+pred_pose_w_z = add_z_coord_for_evaluation(pred_pose)
 
 # print(pred_location.shape)
 # print(pred_pose)
 # print(gt_pose[:, 3:])
 # compute absolute trajectory error (ATE)
-ate,aligned_location = utils.compute_ate(pred_location,gt_location) 
-print('{}, ate: {}'.format(name,ate))
+trans_ate, rot_ate = utils.compute_ate(pred_pose_w_z,gt_pose_w_z) 
+print('{}, translation ate: {}'.format(name,trans_ate))
+print('{}, rotation ate: {}'.format(name,rot_ate))
 
 # color in visulization
 colors = []

utils/geometry_utils.py
@@ -271,22 +271,26 @@ def compute_ate(output,target):
     """
     compute absolute trajectory error for avd dataset
     Args:
-        output: <Nx3> predicted trajectory positions, where N is #scans
-        target: <Nx3> ground truth trajectory positions
+        output: <Nx4> predicted trajectory positions, where N is #scans
+        target: <Nx4> ground truth trajectory positions
     Returns:
-        trans_error: <N> absolute trajectory error for each pose
-        output_aligned: <Nx3> aligned position in ground truth coord
+        trans_ate: <N> translation absolute trajectory error for each pose
+        rot_ate: <N> rotation absolute trajectory error for each pose
     """
-    R,t = rigid_transform_kD(output,target)
-    output_aligned = np.matmul(R , output.T) + t
-    output_aligned = output_aligned.T
+    output_location = output[:, :3]
+    target_location = target[:, :3]
+    R,t = rigid_transform_kD(output_location,target_location)
+    location_aligned = np.matmul(R , output_location.T) + t
+    location_aligned = location_aligned.T
+    yaw_aligned = np.arctan2(R[1,0],R[0,0]) + output[:, 3]
 
-    align_error = np.array(output_aligned - target)
-    trans_error = np.sqrt(np.sum(align_error**2,1))
+    trans_error = np.linalg.norm(location_aligned - target_location, axis=1)
+    rot_error = np.linalg.norm(yaw_aligned - target[:, 3])
     
-    ate = np.sqrt(np.dot(trans_error,trans_error) / len(trans_error))
+    trans_ate = np.sqrt(np.mean(trans_error))
+    rot_ate = np.sqrt(np.mean(rot_error))
 
-    return ate,output_aligned
+    return trans_ate, rot_ate
 
 def remove_invalid_pcd(pcd):
     """
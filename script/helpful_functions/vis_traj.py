cbadc6180b5123e6f680073ae31684b4e1766b6b
Xinhao Liu <gavinliu_hm@hotmail.com>
2022-03-30 15:03:56 -0400

    update trajectory plot

--- script/helpful_functions/vis_traj.py
+++ script/helpful_functions/vis_traj.py
@@ -4,19 +4,24 @@ sys.path.insert(0, '../../')
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset_loader import KITTI
-from matplotlib import cm, colors
+from matplotlib import cm, colors, rc
 
+rc('image', cmap='rainbow')
 data_dir = '../../data/kitti'
 traj = "2011_09_26_drive_0005_sync"
+print("loading dataset")
 dataset = dataset = KITTI(data_dir, traj, 1)
+print("ploting")
 location = dataset.gt_pose
 t = np.arange(location.shape[0]) / location.shape[0]
 location[:, 0] = location[:, 0] - np.mean(location[:, 0])
 location[:, 1] = location[:, 1] - np.mean(location[:, 1])
+u = np.cos(location[:, 2]) * 2
+v = np.sin(location[:, 2]) * 2
 # location[:, 1] = -location[:, 1]
 fig, ax = plt.subplots()
 fig.set_size_inches(15, 10)
-ax.scatter(location[:, 0], location[:, 1], c=t, s=6, alpha=0.6, cmap='rainbow')
+ax.quiver(location[:, 0], location[:, 1], u, v, t, scale=10, scale_units='inches', width=2e-3)
 
 ax.axis('equal')
 ax.tick_params(axis='both', labelsize=18)

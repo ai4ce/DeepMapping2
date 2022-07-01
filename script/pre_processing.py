c8f6881535b23dee60e11938aead0e30e95e8ef5
Xinhao Liu <gavinliu_hm@hotmail.com>
2022-07-01 11:56:57 -0400

    update pre-processing

--- script/pre_processing.py
+++ script/pre_processing.py
@@ -19,6 +19,8 @@ parser.add_argument('-m','--metric',type=str,default='point',choices=['point','p
 parser.add_argument('-d','--data_dir',type=str,default='../data/2D/',help='dataset path')
 parser.add_argument('-t','--traj',type=str,default='traj1.txt',help='trajectory file name')
 parser.add_argument('-v','--voxel_size',type=float,default=1,help='size of downsampling voxel grid')
+parser.add_argument('--group_size',type=int,default=4,help='size of group')
+parser.add_argument('--mode',type=str,default="local",help='local or global frame registraion')
 opt = parser.parse_args()
 rc('image', cmap='rainbow_r')
 
@@ -28,11 +30,12 @@ if not os.path.exists(checkpoint_dir):
 utils.save_opt(checkpoint_dir,opt)
 
 print('loading dataset')
-dataset = Kitti(opt.data_dir, opt.traj, opt.voxel_size, group=False, group_size=2, pairwise=False)
+dataset = Kitti(opt.data_dir, opt.traj, opt.voxel_size, group=True, group_size=opt.group_size, pairwise=False)
 n_pc = len(dataset)
 
 pose_est = np.zeros((n_pc, 4),dtype=np.float32)
 print('running icp')
+dataset.group_flag = False
 for idx in tqdm(range(n_pc-1)):
     dst = dataset[idx].numpy()
     src = dataset[idx+1].numpy()
@@ -61,6 +64,7 @@ print('{}, translation ate: {}'.format(opt.name,trans_ate))
 print('{}, rotation ate: {}'.format(opt.name,rot_ate))
 
 print("Running pairwise registraion")
+dataset.group_flag = True
 pose_est = np.zeros((n_pc, opt.group_size-1, 4),dtype=np.float32)
 for idx in tqdm(range(n_pc)):
     for group_idx in range(1, opt.group_size):
--- script/run_pre_processing.sh
+++ script/run_pre_processing.sh
@@ -5,7 +5,7 @@ DATA_DIR=../data/kitti
 # trajectiory file name
 TRAJ=2011_09_30_drive_0018_sync_raw
 # experiment name, the results will be saved to ../results/2D/${NAME}
-NAME=KITTI_0018_poor_group
+NAME=KITTI_0018
 # subsample rate
 VOXEL=1
 # Group size
@@ -13,5 +13,4 @@ GROUP_SIZE=4
 # Mode
 MODE=local
 
-python incremental_icp.py --name $NAME -d $DATA_DIR -t $TRAJ -v $VOXEL 
-python pairwise_icp.py --name $NAME -d $DATA_DIR -t $TRAJ -v $VOXEL --group_size $GROUP_SIZE --mode $MODE
+python pre_processing.py --name $NAME -d $DATA_DIR -t $TRAJ -v $VOXEL --group_size $GROUP_SIZE --mode $MODE

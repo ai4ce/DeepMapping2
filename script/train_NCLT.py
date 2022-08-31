7f11727fbcd0d62c7fabe489cfa0eedc1930b182
Xinhao Liu <gavinliu_hm@hotmail.com>
2022-08-30 22:49:13 -0400

    fix typo

--- script/train_NCLT.py
+++ script/train_NCLT.py
@@ -168,7 +168,7 @@ for epoch in range(starting_epoch, opt.n_epochs):
     utils.plot_curve(trans_ates, "translation_ate", checkpoint_dir)
     utils.plot_curve(rot_ates, "rotation_ate", checkpoint_dir)
     # utils.plot_curve(training_losses, "training_loss", checkpoint_dir)
-    utils.plot_loss(training_loss, bce_losses, ch_losses, eu_losses, "training_loss", checkpoint_dir)
+    utils.plot_loss(training_losses, bce_losses, ch_losses, eu_losses, "training_loss", checkpoint_dir)
 
     if training_loss_epoch < best_loss:
         print("lowest loss:", training_loss_epoch)
